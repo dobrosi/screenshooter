f4e29f9c37b77556c5349e8c6f2fdff78506d97b
dobrosi <dobrosi@gmail.com>
2023-12-29 12:53:21 +0100

    Update screenshooter.sh

--- screenshooter.sh
+++ screenshooter.sh
@@ -3,4 +3,4 @@ if [[ $1 == "-w" ]]
 then
         args="-u"
 fi
-scrot $args $HOME/Desktop/screenshot.%Y%m%d_%H%M%S.png -e 'optipng $f;notify-send -i $f "Successful saved to\n$f"'
+scrot $args $HOME/Desktop/screenshot.%Y%m%d_%H%M%S.png -e 'notify-send -i $f "Successful saved to\n$f";optipng $f'
